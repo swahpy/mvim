6da385d7b0c17568f17f1a0e96e014fceaae3998
swahpy <hitwhy@gmail.com>
2025-02-17 09:58:39 +0800

    config proper stop of snippets

--- lua/plugins/mini-snippets.lua
+++ lua/plugins/mini-snippets.lua
@@ -62,3 +62,25 @@ local rhs = function()
   snippets.expand { match = false }
 end
 map("i", "<C-g><C-j>", rhs, { desc = "Expand all" })
+
+-- Stop all sessions on Normal mode exit
+local make_stop = function()
+  local au_opts = { pattern = "*:n", once = true }
+  au_opts.callback = function()
+    while snippets.session.get() do
+      snippets.session.stop()
+    end
+  end
+  vim.api.nvim_create_autocmd("ModeChanged", au_opts)
+end
+local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
+vim.api.nvim_create_autocmd("User", opts)
+
+-- Stop session immediately after jumping to final tabstop ~
+local fin_stop = function(args)
+  if args.data.tabstop_to == "0" then
+    snippets.session.stop()
+  end
+end
+local au_opts = { pattern = "MiniSnippetsSessionJump", callback = fin_stop }
+vim.api.nvim_create_autocmd("User", au_opts)
