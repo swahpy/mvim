5369dee1de76174a5fe56a59ac26a5d83ecd09e4
swahpy <hitwhy@gmail.com>
2025-02-18 10:52:37 +0800

    enable input and image module in snacks

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -1,7 +1,10 @@
 local map = vim.keymap.set
 local snacks = require "snacks"
 
-snacks.setup {}
+snacks.setup {
+  input = { enabled = true },
+  image = { enabled = true },
+}
 
 map("n", "<leader>co", function()
   snacks.bufdelete.other()
@@ -10,3 +13,7 @@ end, { desc = "close all buffers except current one" })
 map("n", "<leader>gb", function()
   snacks.gitbrowse.open()
 end, { desc = "open the repo of the active file in browser" })
+
+map("n", "<leader>ih", function()
+  snacks.image.hover()
+end, { desc = "hover image under cursor" })
