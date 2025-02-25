34b11130cdc53b072a81a4200e823ad1d6d24b27
swahpy <hitwhy@gmail.com>
2025-02-25 11:20:16 +0800

    copy yank content to system clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -5,7 +5,6 @@ o.relativenumber = true
 o.scrolloff = 2
 o.winblend = 0
 
-
 -- editing
 o.tabstop = 2
 o.shiftwidth = 2
@@ -20,7 +19,10 @@ o.foldexpr = "nvim_treesitter#foldexpr()"
 -- o.foldmethod = "indent" -- Set 'indent' folding method
 o.foldlevel = 1 -- Display all folds except top ones
 o.foldnestmax = 10 -- Create folds only for some number of nested levels
-vim.g.markdown_folding = 1   -- Use folding by heading in markdown files
+vim.g.markdown_folding = 1 -- Use folding by heading in markdown files
 if vim.fn.has "nvim-0.10" == 1 then
   o.foldtext = "" -- Use underlying text with its highlighting
 end
+
+-- behaviors
+o.clipboard = "unnamedplus"
