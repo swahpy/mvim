76c47fe1bf57b8f6949f36879b90fb3a7e82908d
swahpy <hitwhy@gmail.com>
2025-06-10 09:29:55 +0800

    add keymaps and enable mini move and trailspace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("i", "<C-a>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map({ "n", "x", "v" }, "gb", "^", { desc = "move to beginning character of current line" })
+map({ "n", "x", "v" }, "ge", "$", { desc = "move to ending character of current line" })
+
+map({ "n", "v" }, ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "i", "v" }, "<A-w>", "<cmd> w <cr><Esc>")
+
+map("n", "<A-->", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+map("t", "<A-->", "<cmd>close<cr>", { desc = "Close Terminal" })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,11 @@
+local map = vim.keymap.set
+
+local mini_basic_setup = function(_, opts)
+  require("mini.basics").setup()
+  map("n", "gO", "v:lua.MiniBasics.put_empty_line(v:true)", { expr = true, desc = "Put empty line above" })
+  map("n", "go", "v:lua.MiniBasics.put_empty_line(v:false)", { expr = true, desc = "Put empty line below" })
+end
+
 -- configuration for mini.keymap
 local mini_keymap_setup = function(_, opts)
   local keymap = require("mini.keymap")
@@ -41,95 +49,25 @@ local mini_keymap_setup = function(_, opts)
   map_combo({ "n", "x" }, "hh", "g^")
 end
 
-local mini_completion_setup = function(_, opts)
-  local completion = require("mini.completion")
-  local opts = { filtersort = "fuzzy" }
-  local process_items = function(items, base)
-    return completion.default_process_items(items, base, opts)
-  end
-
-  completion.setup({
-    window = {
-      info = { border = "double" },
-      signature = { border = "double" },
-    },
-    lsp_completion = {
-      source_func = "omnifunc",
-      auto_setup = false,
-      lsp_completion = { process_items = process_items },
-    },
-    mappings = {
-      force_twostep = "<C-t>",
-      force_fallback = "<A-f>",
-    },
-  })
-
-  -- Use fuzzy matching for built-in completion
-  if vim.fn.has("nvim-0.11") == 1 then
-    vim.opt.completeopt:append("fuzzy")
-  end
-
-  -- add icon to completion items
-  require("mini.icons").tweak_lsp_kind()
+local mini_move_setup = function(_, opts)
+  local move = require("mini.move")
+  move.setup()
 end
 
-local mini_snippet_setup = function(_, opts)
-  local snippets = require("mini.snippets")
-  local gen_loader = snippets.gen_loader
-  local map = vim.keymap.set
-
-  snippets.setup({
-    -- Array of snippets and loaders (see |snippets.config| for details).
-    -- Nothing is defined by default. Add manually to have snippets to match.
-    snippets = {
-      -- Load custom file with global snippets first
-      gen_loader.from_file("~/.config/nvim/snippets/global.json"),
-
-      -- Load snippets based on current language by reading files from
-      -- "snippets/" subdirectories from 'runtimepath' directories.
-      gen_loader.from_lang(),
-    },
-
-    -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = { jump_next = "", jump_prev = "" },
-  })
-
-  -- integrate snippets in completion
-  snippets.start_lsp_server()
-
-  -- select from all available snippets in current context
-  local rhs = function()
-    snippets.expand({ match = false })
-  end
-  map("i", "<C-g><C-j>", rhs, { desc = "Expand all" })
-
-  -- Stop all sessions on Normal mode exit
-  local make_stop = function()
-    local au_opts = { pattern = "*:n", once = true }
-    au_opts.callback = function()
-      while snippets.session.get() do
-        snippets.session.stop()
-      end
-    end
-    vim.api.nvim_create_autocmd("ModeChanged", au_opts)
-  end
-  local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
-  vim.api.nvim_create_autocmd("User", opts)
-
-  -- Stop session immediately after jumping to final tabstop ~
-  local fin_stop = function(args)
-    if args.data.tabstop_to == "0" then
-      snippets.session.stop()
-    end
-  end
-  local au_opts = { pattern = "MiniSnippetsSessionJump", callback = fin_stop }
-  vim.api.nvim_create_autocmd("User", au_opts)
+local mini_trailspace_setup = function(_, opts)
+  local move = require("mini.trailspace")
+  move.setup()
+  local rhs = "<cmd>lua MiniTrailspace.trim()<cr>"
+  map("n", "<leader>ts", rhs, { desc = "Trim all trailing whitespaces" })
+  rhs = "<Cmd>lua MiniTrailspace.trim_last_lines()<CR>"
+  map("n", "<leader>tl", rhs, { desc = "Trim all trailing empty lines" })
 end
 
 local mini_setup = function(_, opts)
   mini_keymap_setup(_, opts)
-  -- mini_completion_setup(_, opts)
-  -- mini_snippet_setup(_, opts)
+  mini_move_setup(_, opts)
+  mini_trailspace_setup(_, opts)
+  mini_basic_setup(_, opts)
 end
 
 return {
