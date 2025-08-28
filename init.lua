7898ec08be93b17cedc24b596ad5fa0bc9e12866
swahpy <hitwhy@gmail.com>
2025-08-28 21:13:12 +0800

    replace oil with mini.files

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require("plugins.treesitter")
 require("plugins.mini")
-require("plugins.oil")
 require("plugins.undotree")
 require("plugins.lsp")
 require("plugins.conform")
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -173,6 +173,122 @@ completion.setup({
   },
 })
 
+--> mini.files
+local files = require("mini.files")
+local cmd = vim.api.nvim_create_autocmd
+files.setup({
+  content = {
+    -- filter hidden files on explorer opening.
+    filter = function(fs_entry)
+      return not vim.startswith(fs_entry.name, ".")
+    end,
+  },
+  mappings = {
+    synchronize = "w",
+  },
+  options = {
+    permanent_delete = false,
+  },
+})
+-- toggle files
+map("n", "-", function()
+  if not files.close() then
+    files.open(vim.api.nvim_buf_get_name(0))
+  end
+end, { desc = "open mini files" })
+-- open preview
+map("n", "<C-p>", function()
+  files.refresh({ windows = { preview = true, width_preview = 80 } })
+end, { desc = "enable file content preview" })
+-- toggle hidden files
+local show_dotfiles = false
+local filter_show = function(_)
+  return true
+end
+local filter_hide = function(fs_entry)
+  return not vim.startswith(fs_entry.name, ".")
+end
+local toggle_dotfiles = function()
+  show_dotfiles = not show_dotfiles
+  local new_filter = show_dotfiles and filter_show or filter_hide
+  files.refresh({ content = { filter = new_filter } })
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    map("n", "g.", toggle_dotfiles, { buffer = buf_id })
+  end,
+})
+local map_split = function(buf_id, lhs, direction, close_on_file)
+  local rhs = function()
+    local new_target_window
+    local cur_target_window = files.get_explorer_state().target_window
+    if cur_target_window ~= nil then
+      vim.api.nvim_win_call(cur_target_window, function()
+        vim.cmd("belowright " .. direction .. " split")
+        new_target_window = vim.api.nvim_get_current_win()
+      end)
+      files.set_target_window(new_target_window)
+      files.go_in({ close_on_file = close_on_file })
+    end
+  end
+  local desc = "Open in " .. direction .. " split"
+  if close_on_file then
+    desc = desc .. " and close"
+  end
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+cmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    map_split(buf_id, "_", "horizontal", false)
+    map_split(buf_id, "|", "vertical", false)
+  end,
+})
+-- set custom bookmarks
+local set_mark = function(id, path, desc)
+  files.set_bookmark(id, path, { desc = desc })
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesExplorerOpen",
+  callback = function()
+    set_mark("c", vim.fn.stdpath("config"), "Config") -- path
+    set_mark("w", vim.fn.getcwd, "Working directory") -- callable
+    set_mark("~", "~", "Home directory")
+  end,
+})
+-- Set focused directory as current working directory
+local set_cwd = function()
+  local path = (files.get_fs_entry() or {}).path
+  if path == nil then
+    return vim.notify("Cursor is not on valid entry")
+  end
+  vim.fn.chdir(vim.fs.dirname(path))
+end
+-- Yank in register full path of entry under cursor
+local yank_path = function()
+  local path = (files.get_fs_entry() or {}).path
+  if path == nil then
+    return vim.notify("Cursor is not on valid entry")
+  end
+  vim.fn.setreg(vim.v.register, path)
+end
+-- Open path with system default handler (useful for non-text files)
+local ui_open = function()
+  vim.ui.open(files.get_fs_entry().path)
+end
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local b = args.data.buf_id
+    vim.keymap.set("n", "g~", set_cwd, { buffer = b, desc = "Set cwd" })
+    vim.keymap.set("n", "gX", ui_open, { buffer = b, desc = "OS open" })
+    vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+  end,
+})
+
 --> mini.indentscope
 local indent = require("mini.indentscope")
 indent.setup({
@@ -408,7 +524,7 @@ vim.api.nvim_create_autocmd("User", opts)
 --> mini.trailspace
 local trailspace = require("mini.trailspace")
 trailspace.setup()
-local cmd = "<cmd>lua MiniTrailspace.trim()<cr>"
+cmd = "<cmd>lua MiniTrailspace.trim()<cr>"
 map("n", "<leader>ts", cmd, { desc = "Trim all trailing whitespaces" })
 cmd = "<Cmd>lua MiniTrailspace.trim_last_lines()<CR>"
 map("n", "<leader>tl", cmd, { desc = "Trim all trailing empty lines" })
