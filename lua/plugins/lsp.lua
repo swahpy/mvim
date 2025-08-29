23d65c0c2ebb27f566c8f7ffe999e8dd890c53a2
swahpy <hitwhy@gmail.com>
2025-08-29 15:33:06 +0800

    add basedpyright lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ require("mason").setup()
 
 -- All language servers are expected to be installed with 'mason.vnim'
 vim.lsp.enable({
+  "basedpyright",
   "emmet_language_server",
   "gopls",
   "lua_ls",
